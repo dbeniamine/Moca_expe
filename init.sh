ae6441e51d0c5a8ae9a50fa5a4e9ed6d558bbcd8
David Beniamine <david@beniamine.net>
2016-01-14 16:00:44 +0100

    fix MemProf typo

--- init.sh
+++ init.sh
@@ -8,10 +8,10 @@ wget $archiveurl
 if [ $? -ne 0 ]
 then
     echo "It seems to be an issue with wget, please download the archive;
-    $archiveurl on the root of this repository"
+    '$archiveurl' to the root of this repository"
 fi
 echo "Patching MemProf module"
-cd Memprof/module
+cd MemProf/module
 sed -i 's/\(int max_cnt_op =\).*;/\1 0x1FFFF0;/' mod-memprof.c
 cd -
 echo "You're all set !"
